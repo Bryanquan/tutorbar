448d5c0fd83cf2896947f683ecbd2d994182c1ca
Branquan <woaihuangtuo3344>
2017-11-18 16:05:39 +0800

    Refator commonutil

--- tutorbar/Java/com/util/CommonUtil.java
+++ tutorbar/Java/com/util/CommonUtil.java
@@ -4,16 +4,14 @@ public class CommonUtil {
 
     public static boolean isNull(Object object) {
 
-        return object == null ? true : false;
+        return object == null;
     }
 
     public static boolean isnotNull(Object object) {
-        return object == null ? false : true;
+        return object != null;
     }
 
     public static <E> E cast(Object object) {
         return (E) object;
     }
-
-
 }
