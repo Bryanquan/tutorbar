2f8e7557a9758663f601fa9270119974b431227e
Branquan <woaihuangtuo3344>
2017-12-15 15:42:36 +0800

    Class Format

--- tutorbar/Java/com/action/adminAction.java
+++ tutorbar/Java/com/action/adminAction.java
@@ -9,14 +9,14 @@ import org.apache.struts2.convention.annotation.Results;
 import javax.annotation.Resource;
 
 @Namespace(value = "admin")
-@Results({@Result(name = "admin_list", location = "/Mannager/Admin/admin_list.jsp"),
+@Results({@Result(name = "findAll", location = "/Mannager/Admin/admin_list.jsp"),
         @Result(name = "failure", location = "/Mannager/Admin/failure.jsp")})
 public class adminAction {
     @Resource
     private AdminManager adminManager;
 
-    @Action(value = "/admin_list")
-    public String list() {
-        return "admin_list";
+    @Action(value = "/findAll")
+    public String findAll() {
+        return "findAll";
     }
 }
